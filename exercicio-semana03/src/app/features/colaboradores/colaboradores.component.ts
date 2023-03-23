1e929e9f9801ec440585ef35b43edca73af2539d
Aline Silva <aline.bsbsouza@gmail.com>
2023-03-23 20:07:28 +0000

    Interpolaçao, criação e estilização do componente -colaboradores-

--- exercicio-semana03/src/app/features/colaboradores/colaboradores.component.ts
+++ exercicio-semana03/src/app/features/colaboradores/colaboradores.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./colaboradores.component.scss']
 })
 export class ColaboradoresComponent {
+  
+  titulo = "Colaboradores";
+
+  colaborador = {
+    id: 7,
+    nome:'Nathan Carlos Exercicio',
+    salario: 4500,
+    cargo: 'Dev PI'
+  }
+
+  colaborador02 = {
+    id: 8,
+    nome: 'Maria Art',
+    salario: 4100,
+    cargo: 'Ux/Ui'
+  }
+
+  colaborador03 = {
+    id: 9,
+    nome: 'Ana Blue',
+    salario: 3900,
+    cargo: 'Tech Recruiter'
+  }
 
 }
+
+
