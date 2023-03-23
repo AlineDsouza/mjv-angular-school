34e73ce27f352f65be31bb096866e419661541de
Aline Silva <aline.bsbsouza@gmail.com>
2023-03-23 17:01:23 +0000

    Estrutura app

--- exercicio-semana03/src/app/features/features.module.ts
+++ exercicio-semana03/src/app/features/features.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColaboradoresComponent } from './colaboradores/colaboradores.component';
-import { SellComponent } from './sell/sell.component';
-
 
 
 @NgModule({
   declarations: [
     ColaboradoresComponent,
-    SellComponent
+  
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ColaboradoresComponent
   ]
 })
 export class FeaturesModule { }
