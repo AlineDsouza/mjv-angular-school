f4ad779409142ec85043d850822cb15b406a3ed4
Aline <aline.bsbsouza@gmail.com>
2023-04-03 11:36:14 +0100

    Delete exercicio-semana03 - Copia directory

--- exercicio-semana03 - Copia/src/app/shared/directives/replacecolor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReplacecolorDirective } from './replacecolor.directive';
-
-describe('ReplacecolorDirective', () => {
-  it('should create an instance', () => {
-    //const directive = new ReplacecolorDirective();
-   // (directive).toBeTruthy();
-  });
-});
